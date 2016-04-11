52edfe1fc50300b3ed267ce25d62f0807d1d03ac
Rooftrellen <jzyjiangzhengyao@gmail.com>
2016-04-11 09:58:57 +0800

    Android Experiment Version

---
+++ unityCode/Manager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+	
+	Move move;
+    Text text;
+    Animator text_animator;
+	int i=0;
+
+	void Start () {
+		this.name = "Manager";
+		move = GameObject.Find ("Manager").GetComponent<Move>();
+		text = GameObject.Find ("Text").GetComponent<Text>();
+        text_animator = GameObject.Find("Text").GetComponent<Animator>();
+    }
+
+	void Update () {
+	}
+
+	private void trainStart(Command command)
+    {
+		switch (command) {
+		case Command.Push:
+            
+			move.moveContinuously (Move.MoveDirection.FORWARD);
+			break;
+        case Command.Natural:
+            
+            move.moveContinuously(Move.MoveDirection.STOP);
+            break;
+        }
+	}
+
+	public void commandMove(string command){
+		switch(command){
+		case "PUSH":
+			move.moveForward();
+			break;
+		case "NEUTRAL":
+			move.stopMove ();
+			break;
+		}
+	}
+
+	public void trainStop(){
+		move.stopMoveContinuously ();
+	}
+
+	public void setStage(string stage){
+        int int_stage = int.Parse(stage);
+
+        switch (int_stage)
+        {
+            case 1:              
+                text.text = "natural state, don't imagine motion";
+                trainStart(Command.Natural);
+                break;
+            case 2:
+                trainStop();
+                break;
+            case 3:
+                text.text = "imagine to move forward";
+                trainStart(Command.Push);
+                break;
+            case 4:
+                text.text = "";
+                trainStop();
+                break;
+            case 5:
+                break;
+        }
+        text_animator.SetInteger("stage", int_stage);
+
+    }
+}
+
+enum Command
+{
+    Push,
+    Natural
+}
---
+++ unityCode/Move.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public enum MoveDirection{
+		FORWARD,
+		BACKWARD,
+		STOP
+	}
+    Vector3 origin;
+	bool is_trainning=false;
+	MoveDirection move_direction=MoveDirection.STOP;
+	// Use this for initialization
+	void Start () {
+        origin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (move_direction) {
+		case MoveDirection.FORWARD:
+            Quaternion q= transform.FindChild("Head").transform.rotation;
+			transform.position += q*new Vector3 (0f, 0f, 0.1f);
+			break;
+		case MoveDirection.STOP:			
+			break;
+		}
+	}
+
+	public void moveContinuously(MoveDirection direction){
+		move_direction = direction;
+		is_trainning = true;
+	}
+
+
+	public void stopMoveContinuously(){
+		move_direction = MoveDirection.STOP;
+        transform.position = origin;
+        is_trainning = false;
+	}
+
+	public void moveForward(){
+		if(!is_trainning)
+			move_direction = MoveDirection.FORWARD;
+	}
+
+	public void stopMove(){
+		if(!is_trainning)
+			move_direction = MoveDirection.STOP;
+	}
+}
